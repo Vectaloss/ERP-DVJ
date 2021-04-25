8ae8bff8d44e24669d99a695b9e33ba1cef2f2c8
Arnaud SCHWARTZ <arnaud.schwartz@edu.devinci.fr>
2021-04-25 02:29:14 +0200

    DataTables & First Member view

---
+++ ERP-JE/ERP-JE/Models/Member.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERP_JE.Models
+{
+    public class Member
+    {
+        [Required]
+        public int IdMember { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+        public DateTime Birth { get; set; }
+        public string Email { get; set; }
+        public string SchoolEmail { get; set; }
+        public string Phone { get; set; }
+        public string Promo { get; set; }
+        public string School { get; set; }
+        public string Spe { get; set; }
+        public string Picture { get; set; }
+        public string Cotisation { get; set; }
+        public string Nationality { get; set; }
+        public string IdAdhestionBulletin { get; set; }
+        public string IdAdress { get; set; }
+        public string IdUser { get; set; }
+
+        public Member(int id, string prenom, string Nom)
+        {
+            IdMember = id;
+            FirstName = prenom;
+            LastName = Nom;
+        }
+    }
+}   
--- ERP-JE/ERP-JE/Startup.cs
+++ ERP-JE/ERP-JE/Startup.cs
@@ -27,9 +27,7 @@ namespace ERP_JE
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
